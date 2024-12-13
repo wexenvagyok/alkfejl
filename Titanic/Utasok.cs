8905fffccdf5cd59c298f7130defa4094f76c9d5
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-12-13 08:16:44 +0100

    Titanic-kész

---
+++ Titanic/Utasok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanic
+{
+    internal class Utasok
+    {
+        //ferfiak-masodosztaly;14;154 
+        public string kategoria;
+        public int tulelok;
+        public int eltuntek;
+        public Utasok(string sor)
+        {
+            string[] vag = sor.Split(';');
+            kategoria = vag[0];
+            tulelok = Convert.ToInt32(vag[1]);
+            eltuntek = Convert.ToInt32(vag[2]);
+        }
+    }
+}
