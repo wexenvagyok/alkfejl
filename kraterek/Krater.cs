6a271b25ba7263b1ab75b8152a8dbf39479129eb
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2025-01-22 14:18:57 +0100

    Add files via upload

---
+++ kraterek/Krater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kraterek
+{
+    internal class Krater
+    {
+        public double x;
+        public double y;
+        public double r;
+        public string nev;
+
+        public Krater(string sor)
+        {
+            string[] vag = sor.Split('\t');
+            x = double.Parse(vag[0]);
+            y = double.Parse(vag[1]);
+            r = double.Parse(vag[2]);
+            nev = vag[3];
+        }
+
+        public override string ToString()
+        {
+            return $"A(z) {nev} középpontja X={x} Y={y} sugara R={r}.";
+        }
+    }
+}
---
+++ kraterek/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace kraterek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("felszin_tvesszo.txt");
+
+            List<Krater> kraterek = new List<Krater>();
+
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                kraterek.Add(new Krater(sorok[i]));
+            }
+
+            Console.WriteLine("2. feladat\n A kráterek száma: {0}",kraterek.Count);
+            Console.WriteLine("3. feladat");
+            Console.WriteLine("Kérem egy kráter nevét!!!!!!!!!!!:   ");
+            string nev = Console.ReadLine();
+
+            //segédváltozó
+            string szoveg = "Nincs ilyen kráter";
+
+            for (int i = 0;i < kraterek.Count;i++)
+            {
+                if (kraterek[i].nev == nev)
+                {
+                    szoveg = kraterek[i].ToString();
+                }
+            }
+            Console.WriteLine(szoveg);
+        }
+    }
+}
