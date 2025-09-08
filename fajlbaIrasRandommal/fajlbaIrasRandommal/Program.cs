a89131620654e9449f1fea83ebbdb6080344851d
unknown <farkas.balazs.22@ady-nagyatad.lan>
2025-09-08 10:35:29 +0200

    random

---
+++ fajlbaIrasRandommal/fajlbaIrasRandommal/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace fajlbaIrasRandommal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            List <string> szamok = new List<string>();
+            string szamjegy = "";
+            File.WriteAllText("szamok.txt", "");
+
+            for (int i = 0; i < 12000; i++)
+            {
+                szamjegy = "";
+                for (int j = 0; j < 22; j++)
+                {
+                    szamjegy += rnd.Next(0, 10).ToString();
+                }
+                szamok.Add(szamjegy);
+                File.AppendAllText("szamok.txt", szamjegy + "\n");
+
+                Console.WriteLine(szamjegy.ToString() + "\n");
+            }
+            
+            
+        }
+    }
+}
