5715c097806a92ee818675708348d35dcb087c34
unknown <farkas.balazs.22@ady-nagyatad.hu>
2026-01-16 09:40:02 +0100

    órai

---
+++ egeszSzamok/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace egeszSzamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             olyan class ami egész számokat tárol egy listában, eg tömböt átadva, listában tarolja, az operatoroknal az objektumokat összeadjuk akkor a
+            listakat fuzze össze, ha szamot akkor fuzze hozza a lisahoz azt a szamot, ha szamhoz adjuk hozza akkor a lista összeget adja a szamhoz, kivonás, objektumból
+            másik objektum, a masodik objektum szamainak eltavolitasa az elso listából, ha tobb van az elsoben akkor csak 1db ot vegyen ki, ha szamot vonunk ki akkor azt a szamot
+            törölje ki az elso olyan szamot objektumból, ha szambol akkor a lista összeget, szorzas, ha két objektumot összeszorzunk, akkor a lista elemeit szorozza össze, ha valamelyik
+            nem egyforma hosszu akkor ott ne legyen elem, ha szammal szorozzuk akkor a lista minden elemet megszorozzuk azzal a szammal, ha szamot szorzunk akkor az összegel
+            (lista elemeinek összegével) szorozzuk meg
+            */
+
+            Szamok sz1 = new Szamok(new int[] { 1, 2, 3, 4, 5 });
+            Szamok sz2 = new Szamok(new int[] { 2, 3, 6, 8});
+
+            Console.WriteLine(sz1 + sz2);
+            Console.WriteLine(sz1);
+            Console.WriteLine(sz1 + 200);
+            Console.WriteLine(1 + sz1);
+
+        }
+    }
+}
---
+++ egeszSzamok/Szamok.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egeszSzamok
+{
+    internal class Szamok
+    {
+        List<int> szamok;
+
+        public Szamok(int[] szamok)
+        {
+            this.szamok = new List<int>(szamok);
+        }
+
+        public static Szamok operator +(Szamok egyik, int masik)
+        {
+            List<int> szamok = new List<int>(egyik.szamok);
+            szamok.Add(masik);
+            return new Szamok(szamok.ToArray());
+        }
+
+        public static Szamok operator +(Szamok egyik, Szamok masik)
+        {
+            var szamok = new List<int>(egyik.szamok);
+
+            foreach (int szam in masik.szamok)
+            {
+                szamok.Add(szam);
+            }
+            return new Szamok(szamok.ToArray());
+        }
+
+        public static int operator +(int szam, Szamok szamok)
+        {
+            /*int osszeg = 0;
+
+            foreach(int egySzam in szamok.szamok)
+            {
+                osszeg += szam;
+            }
+
+            int hossz = szamok.szamok.Sum();*/
+            return szam + szamok.szamok.Sum();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.szamok);
+        }
+    }
+}
