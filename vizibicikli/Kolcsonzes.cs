a2d2aa52518236b3aae51f1c00ff5c7a9752d12d
wexen <farkas.balazs.22@F4-45>
2026-04-15 11:26:44 +0200

    vizi érettségi

---
+++ vizibicikli/Kolcsonzes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizibicikli
+{
+    internal class Kolcsonzes
+    {
+        public string Nev;
+        public string Jarmu;
+        public int OraKi;
+        public int PercKi;
+        public int OraBe;
+        public int PercBe;
+        public Kolcsonzes(string nev, string jarmu, int oraKi, int percKi, int oraBe, int percBe)
+        {
+            Nev = nev;
+            Jarmu = jarmu;
+            OraKi = oraKi;
+            PercKi = percKi;
+            OraBe = oraBe;
+            PercBe = percBe;
+        }
+
+        public Kolcsonzes(string sor)
+        {
+            string[] vag = sor.Split(';');
+            Nev = vag[0];
+            Jarmu = vag[1];
+            OraKi = int.Parse(vag[2]);
+            PercKi = int.Parse(vag[3]);
+            OraBe = int.Parse(vag[4]);
+            PercBe = int.Parse(vag[5]);
+        }
+
+    }
+}
---
+++ vizibicikli/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vizibicikli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Betolt();
+        }
+
+        public void Betolt()
+        {
+            string[] sorok = File.ReadAllLines("kolcsonzesek.txt");
+        }
+    }
+}
