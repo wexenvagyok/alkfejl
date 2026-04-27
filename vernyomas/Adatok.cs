4ca9bfac56ae5175c05ad59ad5ba51217a6f12af
wexen <farkas.balazs.22@F4-45>
2026-04-27 13:12:49 +0200

    vernyomas v1

---
+++ vernyomas/Adatok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vernyomas
+{
+    internal class Meres
+    {
+        public int napszam;
+        public string napszak;
+        public int szisztoles;
+        public int diasztoles;
+        public int pulzus;
+
+        public Meres(int napszam, string napszak, int szisztoles, int diasztoles, int pulzus)
+        {
+            this.napszak = napszak;
+            this.napszam = napszam;
+            this.szisztoles = szisztoles;
+            this.diasztoles = diasztoles;
+            this.pulzus = pulzus;
+
+        }
+    }
+}
