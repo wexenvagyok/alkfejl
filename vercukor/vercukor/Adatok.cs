3458cb1cd6b63d7b94b5a34dafbf212b856a6fb1
unknown <farkas.balazs.22@ady-nagyatad.lan>
2026-05-12 10:34:41 +0200

    vércukor

---
+++ vercukor/vercukor/Adatok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vercukor
+{
+    internal class Adatok
+    {
+        public int Nap { get; set; }
+        public string Napszak { get; set; }
+        public string Etkezes { get; set; }
+        public double Ertek { get; set; }
+        public Adatok(int nap, string napszak, string etkezes, double ertek)
+        {
+            Nap = nap;
+            Napszak = napszak;
+            Etkezes = etkezes;
+            Ertek = ertek;
+        }
+    }
+}
---
+++ vercukor/vercukor/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace vercukor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int nap = Convert.ToInt32(napsorszam.Text);
+
+            double ertek = Convert.ToDouble(mertErtek.Text);
+
+            if (nap < 1 || nap > 30)
+            {
+                visszajelzes.Text = "1-30 közötti lehet a nap";
+                visszajelzes.Foreground = Brushes.Red;
+                return;
+            }
+
+            if (ertek < 0 || ertek > 40)
+            {
+                visszajelzes.Text = "0-40 közötti lehet az érték";
+                visszajelzes.Foreground = Brushes.Red;
+                return;
+            }
+
+            if (napszak.SelectedItem == null || elottvagyutan.SelectedItem == null)
+            {
+                visszajelzes.Text = "Minden mezőt tölts ki";
+                visszajelzes.Foreground = Brushes.Red;
+                return;
+            }
+
+            string etkezes = ((ComboBoxItem)elottvagyutan.SelectedItem).Content.ToString();
+
+            if (etkezes == "Étkezés előtt")
+            {
+                if (ertek >= 3.9 && ertek <= 5.6) //IDE MAGAS VAGY ALACSONY
+                {
+                    magasVagyAlacsony.Text = "Normális vércukorszint";
+                    magasVagyAlacsony.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    magasVagyAlacsony.Text = "Nem normális vércukorszint";
+                    magasVagyAlacsony.Foreground = Brushes.Red;
+                }
+            }
+            else
+            {
+                if (ertek < 7.8)
+                {
+                    magasVagyAlacsony.Text = "Normális vércukorszint";
+                    magasVagyAlacsony.Foreground = Brushes.Green;
+                }   
+                else
+                {
+                    magasVagyAlacsony.Text = "Nem normális vércukorszint";
+                    magasVagyAlacsony.Foreground = Brushes.Red;
+                }
+            }
+        }
+    }
+}
